9598410e917870b203ac9dfcb7af681860e8ec26
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2021-03-25 12:35:38 +0800

    refactor bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  .action(gendiff);
+  .action((path1, path2) => console.log(gendiff(path1, path2)));
 
 program.parse(process.argv);
 
