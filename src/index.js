036f19255741e9cc0280012b95ace8c6ee8106d7
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2021-03-28 02:15:56 +0800

    build ast

--- src/index.js
+++ src/index.js
@@ -1,31 +1,61 @@
 import _ from 'lodash';
 import parser from './modules/parser';
 
-export default (filepath1, filepath2) => {
+const gendiff = (filepath1, filepath2) => {
   const fileContentBefore = parser(filepath1);
   const fileContentAfter = parser(filepath2);
 
-  const beforeKeys = _.keys(fileContentBefore);
-  const afterKeys = _.keys(fileContentAfter);
-  const sortedKeys = _.orderBy(_.uniq([...beforeKeys, ...afterKeys]));
+  const iter = (before, after) => {
+    const beforeKeys = _.keys(before);
+    const afterKeys = _.keys(after);
+    const sortedKeys = _.orderBy(_.uniq([...beforeKeys, ...afterKeys]));
 
-  const result = sortedKeys.reduce((acc, name) => {
-    const hasToBefore = beforeKeys.includes(name);
-    const hasToAfter = afterKeys.includes(name);
-    let newAcc = '';
-    if (hasToBefore && hasToAfter && fileContentBefore[name] === fileContentAfter[name]) {
-      newAcc = `${acc}    ${name}: ${fileContentBefore[name]}\n`;
-    } else if (hasToBefore && hasToAfter && fileContentBefore[name] !== fileContentAfter[name]) {
-      newAcc = `${acc}  - ${name}: ${fileContentBefore[name]}\n  + ${name}: ${fileContentAfter[name]}\n`;
-    } else if (hasToAfter && !hasToBefore) {
-      newAcc = `${acc}  + ${name}: ${fileContentAfter[name]}\n`;
-    } else if (hasToBefore && !hasToAfter) {
-      newAcc = `${acc}  - ${name}: ${fileContentBefore[name]}\n`;
-    } else {
-      newAcc = acc;
-    }
-    return newAcc;
-  }, '');
+    return sortedKeys.reduce((acc, name) => {
+      const hasToBefore = beforeKeys.includes(name);
+      const hasToAfter = afterKeys.includes(name);
 
-  return `{\n${result}}`;
+      if (hasToBefore && hasToAfter) {
+        const obj = {
+          key: name,
+          type: 'same',
+        };
+        if (_.isObject(before[name]) && _.isObject(after[name])) {
+          obj.children = iter(before[name], after[name]);
+        } else {
+          obj.value = before[name];
+        }
+        return [...acc, obj];
+      } if (hasToBefore && hasToAfter && !_.eq(before, after)) {
+        const obj = {
+          key: name,
+          type: 'changed',
+          value: {
+            before: before[name],
+            after: after[name],
+          },
+        };
+        return [...acc, obj];
+      } if (!hasToBefore && hasToAfter) {
+        const obj = {
+          key: name,
+          type: 'deleted',
+          value: after[name],
+        };
+        return [...acc, obj];
+      } if (hasToBefore && !hasToAfter) {
+        const obj = {
+          key: name,
+          type: 'added',
+          value: before[name],
+        };
+        return [...acc, obj];
+      }
+
+      return acc;
+    }, []);
+  };
+
+  return iter(fileContentBefore, fileContentAfter);
 };
+
+export default gendiff;
