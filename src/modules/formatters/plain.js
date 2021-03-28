f1d403ded842eb4c606c5ab765fa7eb751689db2
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2021-03-28 21:48:29 +0800

    fix plain formatter

--- src/modules/formatters/plain.js
+++ src/modules/formatters/plain.js
@@ -5,28 +5,30 @@ const withQuote = (value) => (typeof value === 'string' ? `'${value}'` : value);
 const complexValue = (value) => (_.isObject(value) ? '[complex value]' : withQuote(value));
 
 const plain = (tree) => {
-  const iter = (innerTree, prevKey) => innerTree.reduce((acc, node) => {
+  const iter = (innerTree, path) => innerTree.reduce((acc, node) => {
     const {
       type, key, value, children,
     } = node;
 
-    if (children !== undefined) {
-      return `${acc}${iter(children, key)}`;
-    }
+    const dot = path.length === 0 ? '' : '.';
 
-    const dot = prevKey.length === 0 ? '' : '.';
+    const complexPath = `${path}${dot}${key}`;
+
+    if (children !== undefined) {
+      return `${acc}${iter(children, complexPath)}`;
+    }
 
     switch (type) {
       case 'changed': {
         const before = complexValue(value.before);
         const after = complexValue(value.after);
-        return `${acc}Property '${prevKey}${dot}${key}' was updated. From ${before} to ${after}\n`;
+        return `${acc}Property '${complexPath}' was updated. From ${before} to ${after}\n`;
       }
       case 'added': {
-        return `${acc}Property '${prevKey}${dot}${key}' was added with value: ${complexValue(value)}\n`;
+        return `${acc}Property '${complexPath}' was added with value: ${complexValue(value)}\n`;
       }
       case 'deleted': {
-        return `${acc}Property '${prevKey}${dot}${key}' was removed\n`;
+        return `${acc}Property '${complexPath}' was removed\n`;
       }
       default:
         return acc;
